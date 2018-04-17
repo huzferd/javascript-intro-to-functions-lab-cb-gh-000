e87eb979fc1b6481f7c8f4e70c2b12f4e9549cea
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 11:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var shout = (text) => { return text.toUpperCase(); }
 var whisper = (text) => { return text.toLowerCase(); }
 var logShout = (text) => { console.log(text.toUpperCase()); }
 var logWhisper = (text) => { console.log(text.toLowerCase()); }
+var sayHiToGrandma = (text) => { 
+  
+}
+
