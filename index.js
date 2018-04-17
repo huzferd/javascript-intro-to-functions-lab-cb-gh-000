44aa6842529e058e94c103fa3188c49ff3679254
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 11:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var whisper = (text) => { return text.toLowerCase(); }
 var logShout = (text) => { console.log(text.toUpperCase()); }
 var logWhisper = (text) => { console.log(text.toLowerCase()); }
 var sayHiToGrandma = (text) => { 
-  
+  let temp = text.toLowerCase();
+  if (text === temp) {
+    return "I can't hear you!"
+  }
 }
 
