aa92a45d468ea64b9c8cc3c9b3b4e1953a5093b7
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 11:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var shout = (text) => { return text.toUpperCase(); }
 var whisper = (text) => { return text.toLowerCase(); }
 var logShout = (text) => { console.log(text.toUpperCase()); }
 var logWhisper = (text) => { console.log(text.toLowerCase()); }
-var sayHiToGrandma = (text) => { 
-  let lower = text.toLowerCase();
-  let upper = text.toUpperCase();
-  if (text === lower) {
+var sayHiToGrandma = (text) => {
+  if (text === text.toLowerCase()) {
     return "I can't hear you!"
   }
 
-  if (text === upper) {
+  if (text === text.toUpperCase()) {
     return "YES INDEED!";
   }
-  
+
   return text === "I love you, Grandma.".toLowerCase() ? "I love you, too." : "";
 }
-
